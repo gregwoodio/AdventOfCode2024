0c64e9f5fb782aea8dd03afdfae71b0cf59b8237
Greg Wood <contact@gregwood.io>
2024-12-18 10:18:04 -0500

    Day 18 part 2

--- Day18/day18.dart
+++ Day18/day18.dart
@@ -15,6 +15,11 @@ class Coord {
 
   @override
   int get hashCode => x.hashCode ^ y.hashCode;
+
+  @override
+  String toString() {
+    return '$x,$y';
+  }
 }
 
 enum Direction {
@@ -40,7 +45,7 @@ class Node {
 Future<void> main() async {
   final input = await File('./Day18/day18_input.txt').readAsLines();
   print(part1(input, 1024, 71));
-  print(part2(input));
+  print(part2(input, 1024, 71));
 }
 
 int part1(List<String> input, int steps, int size) {
@@ -66,8 +71,70 @@ int part1(List<String> input, int steps, int size) {
   return board[size - 1][size - 1].distance;
 }
 
-int part2(List<String> input) {
-  return 0;
+String part2(List<String> input, int steps, int size) {
+  List<List<Node>> board = List.generate(
+    size,
+    (y) => List.generate(
+      size,
+      (x) => Node(Coord(x, y), 99999),
+    ),
+  );
+
+  final inputCoords = input.map((e) {
+    var parts = e.split(',').map(int.parse).toList();
+    return Coord(parts[0], parts[1]);
+  }).toList();
+
+  /// Whether a particular coordinate in the input causes the board to
+  /// be solvable (true), unsolvable (false), or unknown (null).
+  Map<Coord, bool?> inputCoordStatus = {};
+
+  for (var i = 0; i < steps; i++) {
+    // We know that the first `steps` coordinates are solvable.
+    inputCoordStatus[inputCoords[i]] = true;
+  }
+
+  int firstStep = steps;
+  int lastStep = input.length;
+  int currentStep = (firstStep + lastStep) ~/ 2;
+
+  while (currentStep < inputCoords.length) {
+    /// reset distances and corruption
+    for (var row in board) {
+      for (var node in row) {
+        node.distance = 99999;
+        node.isCorrupt = false;
+      }
+    }
+    board[0][0].distance = 0;
+    for (var i = 0; i <= currentStep; i++) {
+      board[inputCoords[i].y][inputCoords[i].x].isCorrupt = true;
+    }
+
+    // try to solve
+    dfs(board, board[0][0]);
+
+    bool solvable = board[size - 1][size - 1].distance != 99999;
+    inputCoordStatus[inputCoords[currentStep]] = solvable;
+
+    // binary search for the first unsolvable coordinate
+    if (solvable) {
+      firstStep = currentStep;
+    } else {
+      lastStep = currentStep;
+    }
+
+    currentStep = (firstStep + lastStep) ~/ 2;
+
+    // some junky break statements
+    if (inputCoordStatus[inputCoords[currentStep]] != null) {
+      break;
+    }
+  }
+
+  return inputCoords
+      .firstWhere((coord) => inputCoordStatus[coord] == false)
+      .toString();
 }
 
 void dfs(List<List<Node>> board, Node current) {
